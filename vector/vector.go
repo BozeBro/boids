97b9b521965ac7b373cbc3f63b0385d666c4c866
Benedict <ben.ozua@gmail.com>
2021-05-20 14:34:46 -0500

    Added equation to find intersection

--- vector/vector.go
+++ vector/vector.go
@@ -33,6 +33,7 @@ func (v *Vector2D) Subtract(v2 Vector2D) {
 }
 
 // RotatePoints Rotates points by an angle theta about an origin point
+// Rotates in-place
 func RotatePoints(theta float64, origin Vector2D, points ...*Vector2D) {
 	sin, cos := math.Sincos(theta)
 	originX, originY := Components(origin)
@@ -43,7 +44,36 @@ func RotatePoints(theta float64, origin Vector2D, points ...*Vector2D) {
 	}
 }
 
+// Ccomponents returns the components of the vector
 func Components(v Vector2D) (x, y float64) {
 	x, y = v.X, v.Y
 	return x, y
+}
+
+// IsIntersect detects if two linesegments intersect.
+// Intersect if 0 <= t <= 1 and 0 <= u <= 1
+// https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+// Points a and b is 1 segment
+// Points c and d is the 2 segment
+func IsIntersect(a, b, c, d Vector2D) (t, u float64, intersected bool) {
+	// Grabbing notation to look like the formula
+	var (
+		x1, y1 = a.X, a.Y
+		x2, y2 = b.X, b.Y
+		x3, y3 = c.X, c.Y
+		x4, y4 = d.X, d.Y
+	)
+	z := 1 / ((x1-x2)*(y3-y4) - (y1-y2)*(x3-x4))
+	t = ((x1-x3)*(y3-y4) - (y1-y3)*(x3-x4)) * z
+	u = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) * z
+	if t >= 0. && t <= 1. && u >= 0. && u <= 1. {
+		intersected = true
+	}
+	return t, u, intersected
+}
+
+// IntersectionPoints finds the actual intersection points.
+// Will return non-valid numbers if t vector was invalid from IsIntersect
+func IntersectionPoint(a, b Vector2D, t float64) (x, y float64) {
+	return a.X + t*(b.X-a.X), a.Y + t*(b.Y-a.Y)
 }
